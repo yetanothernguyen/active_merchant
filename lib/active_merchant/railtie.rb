d3e54af75131ca4fbea7fd6c2f69d4832713debe
Nguyen Vu Nguyen <nvunguyen@gmail.com>
2011-08-06 19:27:37 +0800

    Use Railtie to include view helpers in Rails 3

---
+++ lib/active_merchant/railtie.rb
@@ -0,0 +1,9 @@
+module ActiveMerchant
+	class Railtie < Rails::Railtie
+		initializer 'activemerchant.initialize' do
+			ActiveSupport.on_load(:action_view) do
+				include ActiveMerchant::Billing::Integrations::ActionViewHelper
+			end
+		end
+	end
+end
